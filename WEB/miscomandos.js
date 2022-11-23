b258bbcea983df375e1c93335b3900143d4668b2
Jonathan Medina <medinajonathan08@gmail.com>
2022-11-23 16:14:42 -0300

    "yguygf"

---
+++ WEB/miscomandos.js
@@ -0,0 +1,7 @@
+<script>
+    function usrpas() {
+                if (document.form1.txt.value == "admin" && document.form1.num.value == "1234") {window.location = "index.html"}
+    else {alert("Error en Usuario o Contraseña. Intenta de nuevo.")}
+            }
+    document.oncontextmenu = new Function("return false");
+</script>
